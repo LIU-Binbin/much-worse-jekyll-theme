307f4591b03241a9d1ac6f6a734c869b2671c0f3
bbliu <bbliu@buaa.edu.cn>
2023-05-11 16:55:19 +0800

    h

--- README.md
+++
@@ -1,7 +0,0 @@
-# My website
-
-A place to share my research activities and news. 
-
----
-_Using Academic jekyll theme, avaiable under [MIT License](https://opensource.org/licenses/MIT)._
-
--- assets/js/cite.js
+++
@@ -1,7 +0,0 @@
-function copyCitation() {
-  /* Get the text field */
-  var text = document.getElementById("textCitation");
-
-   /* Copy the text inside the text field */
-  navigator.clipboard.writeText(text.textContent);
-} 
